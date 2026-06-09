e8d03a2a2a1f21aa4ea660538f25855f7b137148
Priyanshu Kumar <priyanshu.devops10@gmail.com>
2026-06-09 13:45:32 +0530

    Added variable, for_each loop, nested map

--- azurerm_resource_group/main.tf
+++ azurerm_resource_group/main.tf
@@ -1,4 +1,5 @@
 resource "azurerm_resource_group" "rg" {
-  name = "rg-1"
-  location = "EastUS"
+  for_each = var.rgs
+  name = each.value.name
+  location = each.value.location
 }
---
+++ azurerm_resource_group/terraform.tfvars
@@ -0,0 +1,10 @@
+rgs = {
+  rg1 = {
+    name     = "rg-1"
+    location = "EastUS"
+  }
+  rg1 = {
+    name     = "rg-1"
+    location = "EastUS"
+  }
+}
